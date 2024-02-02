a4c3805a6d6da728169bcf8eff0e08b201cfc85c
akshaymehtastudio88 <148732672+akshaymehtastudio88@users.noreply.github.com>
2024-02-02 16:18:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+   function menu()
+        {
+            document.getElementById("menu").classList.toggle("icon")
+            document.getElementById("nav-bar").classList.toggle("change")
+        
+        }
